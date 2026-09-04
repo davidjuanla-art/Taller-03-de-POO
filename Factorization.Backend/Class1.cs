974febb4639208521d7cb0a919676b27f1be4d5a
Acer 23 <Acer 23@WINDOUS_32>
2026-09-04 18:23:31 -0500

    Added backend and frontend for exercise 35

---
+++ Factorization.Backend/Class1.cs
@@ -0,0 +1,59 @@
+﻿namespace Factorization.Backend;
+
+    public class Factor
+    {
+        // field
+
+        private int number;
+
+        //constructor
+        public Factor(int number)
+        {
+            Number = number;
+        }
+
+        //properties
+
+        public int Number 
+        { 
+            get => number;
+            set => number = value; }
+         
+
+            //methods
+          public int Factoritation()
+        {
+            do
+            {
+                if (number % 2 == 0)
+                {
+                    number = number / 2;
+                }
+                else if (number % 3 == 0)
+                {
+                    number = number / 3;
+                }
+                else if (number % 5 == 0)
+                {
+                    number = number / 5;
+                }
+                else if (number % 7 == 0)
+                {
+                    number = number / 7;
+                }
+                else if (number % 11 == 0)
+                {
+                    number = number / 11;
+                }
+                else
+                {
+                    break;
+                }
+
+            } while (number > 1);
+
+            return number;
+        }
+
+
+    }
---
+++ Factorization.Fronted/Program.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleExtensio;
+using Factorization.Backend;
+using System.Numerics;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    int number = ConsoleExtension.GetInt("Enter a Number: ");
+
+    Factor factor = new Factor(number);
+
+    int result = factor.Factoritation();
+
+    Console.WriteLine($"Result: {result}");
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions(
+            "Desea continuar? s/n: ",
+            options
+        );
+
+    } while (!options.Any(x =>
+        x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("game over");
